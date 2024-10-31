0d83cdeb608f6eaa84622e52633a99cecaed87a1
Satish Singh <satishsingh12@gmail.com>
2024-10-31 11:10:18 +0530

    change done - Toast.js

--- src/components/GlobalComponents/Toast.js
+++ src/components/GlobalComponents/Toast.js
@@ -4,19 +4,24 @@ import "./styles/toast.css";
 const Toast = ({ msg, handleShow, bgColor }) => {
   const [show, setShow] = useState(true);
 
+  console.log({ msg, handleShow, bgColor });
+
   useEffect(() => {
+    setShow(true); // Reset show to true whenever msg changes
     const timer = setTimeout(() => {
       setShow(false);
     }, 10000);
-
     return () => clearTimeout(timer);
-  }, []);
+  }, [msg]);
 
   // If the toast is not visible, return null to hide it
   if (!show) return null;
 
   return (
-    <div className="toast" style={{ backgroundColor: bgColor }}>
+    <div
+      className="toast"
+      style={{ backgroundColor: bgColor, display: show ? "block" : "none" }}
+    >
       <div className="toast-header">
         <h4>{msg.title}</h4>
         <p className="toast-close-btn" onClick={handleShow}>
